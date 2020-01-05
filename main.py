5dc3fc595bd2aaff6698957e13921885af2d231e
Dziarkowska <anna-dziarkowska@wp.pl>
2020-01-06 00:03:46 +0100

    poprawka load

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-#import receiver_support
 import loadfile
 
 data = loadfile.loadfile('gqrx_20191214_143406_433502000_1800000_fc.raw')
 
-#print(data[1:50])
-=======
-
-nanana
->>>>>>> 2be250c3d2016d60353da0e83c74f2e6c94aa0a7
